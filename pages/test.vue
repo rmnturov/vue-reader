a6cdd4d23023bffb51674da4cdf2e95b1e7f251f
Roman Turov <razorfromrds@mail.ru>
2018-04-27 19:41:47 +0700

    asdasd
    asd

--- pages/test.vue
+++ pages/test.vue
@@ -1,39 +1,39 @@
 <template lang="pug">
-  div
-    h1(title="GHbdtn") Заголовок 1
-    ul(v-if="true")
-      //- div asdasd
-      li(v-for="post in posts")
-        //- img(:src="post.enclosure.url" :alt="post.title")
-        //- img(v-attr="src: post.enclosure.url")
-        h2 {{post.title}}
-        p {{post.content}}
-        div {{ post.isoDate | humanDate }}   
+  //- div
+  //- h1(title="GHbdtn") Заголовок 1
+  //-   ul.feed(v-if="true")
+  //-     li.feed__list-item(v-for="post in posts")
+  //-       a.feeditem__link(:href="post.link")
+  //-         figure.feeditem__img-wrapper
+  //-           img.feeditem__img(v-if="post.enclosure" :src="post.enclosure.url" :alt="post.title")
+  //-         h2.feeditem__header {{post.title}}
+  //-         p.feeditem__description {{post.content}}
+  //-         time.feeditem__date() {{ post.isoDate | humanDate }}
 </template>
 
 <script>
-
 import axios from "axios";
-var parser = require("rss-parser-browser");
+// var parser = require("rss-parser-browser");
 let feedUrl = "http://news.ngs.ru/rss/articles/";
 let corsNowUrl = "https://cors.now.sh/" + feedUrl;
 
 export default {
-  asyncData({ req, error }) {
-    return axios
-      .get(corsNowUrl)
-      .then(res => {
-        var entries;
-        var doc = parser.parseString(res.data, function(err, parsed) {
-          entries = parsed.feed.entries;
-          console.log(entries);
-        });
-        return { posts: entries };
-      })
-      .catch(e => {
-        error({ statusCode: 404, message: "Post not found" });
-      });
-  },
+  // asyncData({ req, error }) {
+  //   return axios
+  //     .get(corsNowUrl)
+  //     .then(res => {
+  //       console.log;
+  //       // var entries;
+  //       var doc = parser.parseString(res.data, function(err, parsed) {
+  //         entries = parsed.feed.entries;
+  //         console.log(parsed);
+  //       });
+  //       return { posts: entries };
+  //     })
+  //     .catch(e => {
+  //       error({ statusCode: 404, message: "Post not found" });
+  //     });
+  // },
   head: {
     title: "sadasdadasd"
   }
@@ -71,7 +71,95 @@ export default {
 </script> 
 
 <style lang="scss">
+*:focus {
+  outline: 2px solid #1c6ea4;
+  outline-offset: 2px;
+}
 a {
   display: inline;
+}
+.feed {
+  position: relative;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+
+  &__list-item {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    float: left;
+    width: 25%;
+    height: 400px;
+  }
+}
+
+.feeditem {
+  &__link {
+    position: relative;
+    display: flex;
+    height: 100%;
+    padding: 30px;
+    flex-direction: column;
+    overflow: hidden;
+    text-decoration: none;
+    color: inherit;
+    border-radius: 6px;
+
+    &:hover {
+      .feeditem__header {
+        color: red;
+        // border-bottom: 1px solid rgba(255, 0, 0, 0.3);
+      }
+    }
+
+    &::after {
+      content: "";
+      position: absolute;
+      width: 100%;
+      height: 60px;
+      right: 0;
+      bottom: 0;
+      background-image: linear-gradient(
+        rgba(255, 255, 255, 0),
+        rgba(255, 255, 255, 0.5) 33%,
+        white
+      );
+    }
+  }
+  &__img-wrapper {
+    box-sizing: border-box;
+    position: relative;
+    display: block;
+    width: 100%;
+    // min-height: 33%;
+    // padding-bottom: 75%;
+    height: 56%;
+    background-color: lightgrey;
+    margin-bottom: 12px;
+  }
+  &__img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+  &__header {
+    // display: inline;
+    font-size: 16px;
+    line-height: 20px;
+  }
+  &__description {
+    font-size: 16px;
+    line-height: 20px;
+  }
+  &__date {
+    font-size: 16px;
+    line-height: 20px;
+    color: grey;
+  }
 }
 </style>
